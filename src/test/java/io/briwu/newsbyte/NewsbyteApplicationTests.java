865beb16dface3a854861465a45b0c67f6394790
Brian Wu <briawu@umich.edu>
2024-06-12 23:07:19 -0400

    Add files via upload

---
+++ src/test/java/io/briwu/newsbyte/NewsbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.briwu.newsbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsbyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
